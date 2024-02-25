73935f5e66a9142423625af6c498dadb65a3f8bc
Gudmundls <gusta6517@oslomet.no>
2024-02-25 14:14:23 +0100

    La til kommentarer

--- src/main/resources/static/Main.js
+++ src/main/resources/static/Main.js
@@ -1,5 +1,5 @@
 let tickets = [];
-
+//fetches values
 function buyTicket() {
     const firstName = document.getElementById('firstName').value;
     const lastName = document.getElementById('lastName').value;
@@ -7,7 +7,7 @@ function buyTicket() {
     const email = document.getElementById('email').value;
     const phone = document.getElementById('phone').value;
     const quantity = document.getElementById('quantity').value;
-
+//checks if values are there and corrext
     if (validateEmail(email) && validatePhone(phone) && validateFirstName(firstName) && validateLastName(lastName) && film && quantity) {
         const ticket = {
             firstName: firstName,
@@ -25,24 +25,28 @@ function buyTicket() {
         alert('Vennligst fyll ut alle feltene med riktige verdier.');
     }
 }
+//regex for first name
 function validateFirstName(firstName) {
     const re = /^[a-zA-ZæøåÆØÅ\- ]+$/;
     return re.test(firstName);
 }
+//regex for last name
+
 function validateLastName(lastName) {
     const re = /^[a-zA-ZæøåÆØÅ\- ]+$/;
     return re.test(lastName);
 }
+//regex for email
 function validateEmail(email) {
     const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return re.test(email);
 }
-
+//regex for phone
 function validatePhone(phone) {
     const re = /^(0047|\+47|47)?\d{8}$/;
     return re.test(phone);
 }
-
+//clears tickets table
 function clearTickets() {
     tickets = [];
     displayTickets();
@@ -51,14 +55,14 @@ function clearTickets() {
 function displayTickets() {
     const ticketList = document.getElementById('ticketList');
     ticketList.innerHTML = '';
-
+//adds tickets to list
     tickets.forEach(ticket => {
         const ticketItem = document.createElement('div');
         ticketItem.textContent = `Navn: ${ticket.firstName} ${ticket.lastName}, Film: ${ticket.film}, E-post: ${ticket.email}, Telefon: ${ticket.phone}, Antall: ${ticket.quantity}`;
         ticketList.appendChild(ticketItem);
     });
 }
-
+//clears forms by changing values to empty
 function clearForm() {
     document.getElementById('firstName').value = '';
     document.getElementById('lastName').value = '';
